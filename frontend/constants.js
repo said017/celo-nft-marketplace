b160069ae6b0bc404133a912470e5cdd3cd42e5a
said017 <saidam017@gmail.com>
2022-09-14 22:47:40 +0700

    remove submodule

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/said017/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0x55C8b1B3Af4822abAD1F86c0967367de8D95558A";
